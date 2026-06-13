f4dbe320a522df1d280a95f7b9106b1a1bd891c3
temecodes <teme.codes@gmail.com>
2026-06-13 17:47:58 +0300

    First working version of repl

--- main.go
+++ main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	//"strings"
+	"os"
 )
 
 
-
-
 func main() {
-	fmt.Println("Hello, World!")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for{
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan(){
+			break
+		}
+		text := scanner.Text()
+		cleanText := cleanInput(text)
+		if len(cleanText) == 0{
+			continue
+		}
+		fmt.Printf("Your command was: %s\n", cleanText[0])
+
+	}
+	return
+
 }
